252dd1574269ff748a974a7030de0436cda5bff2
17603958627 <2236769562@qq.com>
2020-01-11 14:52:12 +0800

    数据库修改；

--- vue_api/sql/db.js
+++ vue_api/sql/db.js
@@ -3,24 +3,24 @@
  */
 const mongoose = require('mongoose'); // 引入模块
 // mongoose.Promise = global.Promise;  
-const DB_URL = "mongodb://localhost:27017/bs1910"; // 数据库连接地址以及连接到哪
+const DB_URL = "mongodb://localhost:27017/sh1911"; // 数据库连接地址以及连接到哪
 
 // 连接数据库
 mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 // 连接成功
 mongoose.connection.on('connected', () => {
-  console.log('数据库连接成功');
+    console.log('数据库连接成功');
 })
 
 // 连接失败断开
 mongoose.connection.on('disconnected', () => {
-  console.log('数据库断开');
+    console.log('数据库断开');
 })
 
 // 连接错误
 mongoose.connection.on('error', () => {
-  console.log('数据库连接异常');
+    console.log('数据库连接异常');
 })
 
 module.exports = mongoose;
